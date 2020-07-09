876fc1898f05d0706e8e617b6118577bf055296f
Elizabeth Hyer <hyerhandmade@gmail.com>
2020-07-09 17:08:15 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,40 @@
+def sort_array_asc(array)
+  array.sort do |a, b|
+    a <=> b 
+  end 
+end 
+
+def sort_array_desc(array)
+  array.sort_by do |sort|
+  -sort
+  end
+end 
+
+def sort_array_char_count(array)
+  array.sort do |a, b| 
+  a.length <=> b.length
+  end
+end 
+
+#def swap_elements(a, b)
+ # a = [1]
+ # b = [2]
+  #array[a], array[b] = array[b], array[a]
+#end 
+
+def reverse_array(array)
+  array.reverse do |a, b|
+    a <=> b 
+  end
+end 
+
+def kesha_maker(array, index_number)
+  kesha = []
+  array.each do { |word| word.gsub([2], " ") }
+    kesha << puts array.insert do ([2], "$")
+  end
+  kesha
+end 
+
+
 
